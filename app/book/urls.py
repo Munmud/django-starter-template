4e156a4cfadb5252fb27734a617ed7cc55ea77be
munmud <moontasir042@gmail.com>
2024-02-02 18:36:08 +0600

    Fix url

--- app/book/urls.py
+++ app/book/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from .views import BookListView, BookDetailedView
 
 urlpatterns = [
+    path('/', BookListView.as_view(), name="bookList"),
     path('', BookListView.as_view(), name="bookList"),
-    path('/<int:pid>', BookDetailedView.as_view(), name="bookDetails")
+    path('/<int:pid>', BookDetailedView.as_view(), name="bookDetails"),
+    path('/<int:pid>/', BookDetailedView.as_view(), name="bookDetails")
 ]
